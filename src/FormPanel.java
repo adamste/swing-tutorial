5212ed28359831c7b6bf44c6d6eb04e5b3dd4456
Adam S <adam.stepien1@gmail.com>
2018-01-02 14:01:51 +0100

    12/77

--- src/FormPanel.java
+++ src/FormPanel.java
@@ -4,10 +4,23 @@ import java.awt.*;
 
 public class FormPanel extends JPanel {
 
+    private JLabel nameLabel;
+    private JLabel occupationLabel;
+    private JTextField nameField;
+    private JTextField occupationField;
+    private JButton okBtn;
+
     public FormPanel(){
         Dimension dimension=getPreferredSize();
         dimension.width=250;
         setPreferredSize(dimension);
+
+        nameLabel=new JLabel("Name: ");
+        occupationLabel=new JLabel("Ocupation: ");
+        nameField=new JTextField(10);
+        occupationField=new JTextField(10);
+        okBtn=new JButton("OK"); 
+
         Border innerBorder=BorderFactory.createTitledBorder("Add Person");
         Border outerBorder=BorderFactory.createEmptyBorder(5,5,5,5);
         setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
