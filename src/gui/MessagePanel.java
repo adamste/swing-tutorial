51decdb2a08f6aced4c9ea2a9770100801abb3f9
Adam S <adam.stepien1@gmail.com>
2018-01-11 11:45:41 +0100

    Working tree with checkboxes

--- src/gui/MessagePanel.java
+++ src/gui/MessagePanel.java
@@ -1,22 +1,21 @@
 package gui;
 
-import com.mysql.fabric.Server;
-
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 
 class ServerInfo {
     private String name;
     private int id;
+    private boolean checked;
 
-    public ServerInfo(String name, int id) {
+    public ServerInfo(String name, int id, boolean checked) {
         this.name = name;
         this.id = id;
+        this.checked=checked;
     }
 
     public int getId() {
@@ -26,20 +25,31 @@ class ServerInfo {
     public String toString() {
         return name;
     }
+
+    public boolean isChecked(){
+        return checked;
+    }
+
+    public void setChecked(boolean checked){
+        this.checked=checked;
+    }
 }
 
 public class MessagePanel extends JPanel {
     private JTree serverTree;
-    private DefaultTreeCellRenderer treeCellRenderer;
+    private ServerTreeCellRendered treeCellRenderer;
+    private ServerTreeCellEditor treeCellEditor;
 
     public MessagePanel() {
-        treeCellRenderer = new DefaultTreeCellRenderer();
-        treeCellRenderer.setLeafIcon(Utils.createIcon("/images/Server16.gif"));
-        treeCellRenderer.setOpenIcon(Utils.createIcon("/images/WebComponentAdd16.gif"));
-        treeCellRenderer.setClosedIcon(Utils.createIcon("/images/WebComponentAdd24.gif"));
+//        treeCellRenderer = new ServerTree();
+        treeCellRenderer=new ServerTreeCellRendered();
+        treeCellEditor=new ServerTreeCellEditor();
 
         serverTree = new JTree(createTree());
         serverTree.setCellRenderer(treeCellRenderer);
+        serverTree.setCellEditor(treeCellEditor);
+        serverTree.setEditable(true);
+
         serverTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
         serverTree.addTreeSelectionListener(new TreeSelectionListener() {
             @Override
@@ -63,16 +73,16 @@ public class MessagePanel extends JPanel {
     private DefaultMutableTreeNode createTree() {
         DefaultMutableTreeNode top = new DefaultMutableTreeNode("Servers");
         DefaultMutableTreeNode branch1 = new DefaultMutableTreeNode("USA");
-        DefaultMutableTreeNode server1 = new DefaultMutableTreeNode(new ServerInfo("New York", 0));
-        DefaultMutableTreeNode server2 = new DefaultMutableTreeNode(new ServerInfo("Boston", 1));
-        DefaultMutableTreeNode server3 = new DefaultMutableTreeNode(new ServerInfo("Los Angeles", 2));
+        DefaultMutableTreeNode server1 = new DefaultMutableTreeNode(new ServerInfo("New York", 0, true));
+        DefaultMutableTreeNode server2 = new DefaultMutableTreeNode(new ServerInfo("Boston", 1, false));
+        DefaultMutableTreeNode server3 = new DefaultMutableTreeNode(new ServerInfo("Los Angeles", 2, true));
         branch1.add(server1);
         branch1.add(server2);
         branch1.add(server3);
 
         DefaultMutableTreeNode branch2 = new DefaultMutableTreeNode("UK");
-        DefaultMutableTreeNode server4 = new DefaultMutableTreeNode(new ServerInfo("London", 3));
-        DefaultMutableTreeNode server5 = new DefaultMutableTreeNode(new ServerInfo("Edinburgh", 4));
+        DefaultMutableTreeNode server4 = new DefaultMutableTreeNode(new ServerInfo("London", 3, false));
+        DefaultMutableTreeNode server5 = new DefaultMutableTreeNode(new ServerInfo("Edinburgh", 4,true));
         branch2.add(server4);
         branch2.add(server5);
 
