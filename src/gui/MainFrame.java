0e064ee94f670545f9f5d055cff89d2ecf5c21b5
Adam S <adam.stepien1@gmail.com>
2018-01-15 12:03:00 +0100

    Git look and feel

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -26,6 +26,16 @@ public class MainFrame extends JFrame {
 
     public MainFrame() {
         super("Hello World");
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Can't set look and feel");
+        }
 
         setLayout(new BorderLayout());
         toolbar = new Toolbar();
